0595db11bd31eb11bb2525cc77ae430efcd766d6
susheelk <susheel.kona@gmail.com>
2021-02-25 18:31:31 -0500

    init

---
+++ SourceFeed.py
@@ -0,0 +1,21 @@
+import feedparser
+import typing
+from datetime import datetime
+
+
+class SourceFeed:
+
+    def __init__(self, url: str, on_complete) -> None:
+        self.url = url
+        self.last_updated: datetime = None
+        self.last_id: str = None
+        self.on_complete = on_complete
+
+    def fetch(self):
+        feed = feedparser.parse(self.url)
+
+        if feed.channel.updated > self.last_updated:
+            pass
+
+        self.last_updated = feed.channel.updated
+        self.on_complete()
