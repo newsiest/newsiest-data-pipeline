abbb09304c01d39c11242847eb51ae89d6cc9652
susheelk <susheel.kona@gmail.com>
2021-02-25 19:26:20 -0500

    filter

--- feed_parsers/source_feed.py
+++ feed_parsers/source_feed.py
@@ -1,7 +1,7 @@
 import feedparser
 import typing
+import abc
 from datetime import datetime
-
 from models.news_article import NewsArticle
 
 
@@ -12,9 +12,8 @@ class SourceFeed:
 
     def __init__(self, url: str, on_complete) -> None:
         self.url = url
-        self.last_updated: datetime = None
-        self.top_article_id: str = None
-        self.last_id: str = None
+        self.last_updated: datetime = datetime(1971, 1, 1)
+        self.top_article_date: datetime = datetime(1971, 1, 1)
         self.on_complete = on_complete
 
     def fetch(self) -> [NewsArticle]:
@@ -25,11 +24,18 @@ class SourceFeed:
         if last_updated <= self.last_updated:
             return []
 
-
+        # Only get new articles
+        articles = self.__parse_articles(feed)
+        articles = filter(lambda art: art.pub_date >= self.top_article_date)
 
         self.last_updated = feed.channel.updated
         self.on_complete()
 
-    def __parse_last_updated(self, feed: feedparser):
+    @abc.abstractmethod
+    def __parse_last_updated(self, feed: feedparser) -> datetime:
+        pass
+
+    @abc.abstractmethod
+    def __parse_articles(self, feed: feedparser) -> [NewsArticle]:
         pass
 
--- models/news_article.py
+++ models/news_article.py
@@ -1,9 +1,9 @@
 import datetime
 
 class NewsArticle:
-    def __init__(self, title: str = None, author: str = None, url: str = None, img_url: str = None, published: datetime = None):
+    def __init__(self, title: str = None, author: str = None, url: str = None, img_url: str = None, pub_date: datetime = None):
         self.title = title
         self.author = author
         self.url = url
         self.img_url = img_url
-        self.published = published
+        self.pub_date = pub_date
