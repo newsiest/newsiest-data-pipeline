22f6cc4771bace5e931ed8c34145e5b1cb1dd3e3
susheelk <susheel.kona@gmail.com>
2021-02-25 19:11:58 -0500

    typing

--- feed_parsers/source_feed.py
+++ feed_parsers/source_feed.py
@@ -2,8 +2,13 @@ import feedparser
 import typing
 from datetime import datetime
 
+from models.news_article import NewsArticle
+
 
 class SourceFeed:
+    """
+    Represents a generic rss feed
+    """
 
     def __init__(self, url: str, on_complete) -> None:
         self.url = url
@@ -12,15 +17,19 @@ class SourceFeed:
         self.last_id: str = None
         self.on_complete = on_complete
 
-    def fetch(self):
+    def fetch(self) -> [NewsArticle]:
         feed = feedparser.parse(self.url)
 
-        if feed.channel.updated > self.last_updated:
-            pass
+        # Only refresh feed if it updated
+        last_updated = self.__parse_last_updated()
+        if last_updated <= self.last_updated:
+            return []
+
+
 
         self.last_updated = feed.channel.updated
         self.on_complete()
 
-    def __parse_last_updated(self):
+    def __parse_last_updated(self, feed: feedparser):
         pass
 
