5fa2d059dcd4c4e1990ec79b4113db44e9aa5a9e
susheelk <susheel.kona@gmail.com>
2021-02-26 23:29:43 -0500

    start scheduling feeds

--- pipeline/feed_manager.py
+++ pipeline/feed_manager.py
@@ -4,6 +4,8 @@ from queue import Queue
 from feed_parsers.source_feed import SourceFeed
 from models.news_article import NewsArticle
 from pipeline.pipeline import PipelineStage
+from apscheduler.schedulers.background import BackgroundScheduler
+import threading
 
 FREQUENCY = 10
 
@@ -15,8 +17,32 @@ class FeedManager(PipelineStage):
         self._feeds = feeds
 
     def start(self):
-        pass
+        scheduler = BackgroundScheduler()
+        scheduler.add_job(self._wait_on_queue, 'interval', seconds=1)
+
+        for f in self.feeds:
+            scheduler.add_job(
+                lambda: self._start_feed_thread(f),
+                'interval',
+                seconds=5
+            )
+
+        scheduler.start()
 
     def _enqueue_one(self, article: NewsArticle):
         pass
+
+    def _wait_on_queue(self):
+        if(not self._queue.empty()):
+            self.emit(self._queue.get())
+
+    def _start_feed_thread(self, feed: SourceFeed):
+        t = threading.Thread(target=FeedManager._start_feed, args=(feed, self._queue))
+        t.start()
+
+    @staticmethod
+    def _start_feed(feed: SourceFeed, queue: Queue):
+        articles = feed.fetch()
+        for a in articles:
+            queue.put(a)
 
