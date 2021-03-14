e6a95059b940bce526177606298d1c802be90d0e
Arjun Vasudevan <vasudevanarjun11@gmail.com>
2021-03-14 11:00:19 -0400

    POS tagging with spacy

--- pipeline/tagger.py
+++ pipeline/tagger.py
@@ -1,5 +1,8 @@
-import nltk
+from collections import Counter
+from string import punctuation
+
 from rake_nltk import Rake
+import spacy
 
 from pipeline.pipeline_stage import PipelineStage
 
@@ -8,13 +11,33 @@ class Tagger(PipelineStage):
 
     def __init__(self):
         super().__init__()
-        nltk.download('stopwords')
-        nltk.download('punkt')
-        self.r = Rake(min_length=1, max_length=2)
+        self.nlp = spacy.load("en_core_web_md")
+        self.pos_tag = ['PROPN', 'NOUN']
+
+        self.r = Rake(max_length=1)
 
     def _process_one(self, to_process):
-        self.r.extract_keywords_from_text(to_process.title)
-        to_process.tags = self.r.get_ranked_phrases()
+        text = (to_process.title + to_process.desc).lower()
+
+        # rake
+        self.r.extract_keywords_from_text(text)
+        rakes = set(self.r.get_ranked_phrases())
+
+        # spacy
+        result = []
+        doc = self.nlp(text)
+
+        for token in doc:
+            if token.text in self.nlp.Defaults.stop_words or token.text in punctuation:
+                continue
+
+            if token.pos_ in self.pos_tag:
+                result.append(token.text)
+
+        spacys = [x[0] for x in Counter(result).most_common()]
+
+        # print(to_process.title, rakes, spacys)
+        # print(to_process.title, list(rakes.intersection(spacys)))
 
     def start(self):
         pass
